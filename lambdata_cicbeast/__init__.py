44c2c5edfd6d4a13b8ab1b43c3ab6ddf4b2081d7
cicbeast <49789241+cicbeast@users.noreply.github.com>
2019-09-06 21:13:54 -0400

    Updated setup.py, __init__.py and Pipfile

--- lambdata_cicbeast/__init__.py
+++ lambdata_cicbeast/__init__.py
@@ -1,35 +1,34 @@
-"""
-lambdata-cicbeast - Collection of DS Helper Functions
-"""
-name = 'lambdata_cicbeast'
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from scipi.stats import chisquare
 
+"""
+lambdata-cicbeast - Collection of DS Helper Functions
+"""
+name = 'lambdata_cicbeast'
+
+
 ZEROS = np.zeros(5)
 ONES = np.ones(10)
 
-# Chi Squared test on Contingency Table
-def cont_chi2 (
-    X, y, ddof=1, dropna=False)
 
-    ctab = pd.crosstab(
-        X, y, dropna=dropna)   
+# Chi Squared test on Contingency Table
+def cont_chi2(X, y, ddof=1, dropna=False):
+
+    ctab = pd.crosstab(X, y, dropna=dropna)
 
     chi2, p = chisquare(
         ctab, ddof=ddof)
 
     return ctab, chi2
 
-# Date Separater
-def date_sep(
-    X, infer_dtf=True)
 
-    X['date'] = pd.to_datetime(
-        X['date'], infer_datetime_format=infer_dtf)
-    
+# Date Separater
+def date_sep(X, infer_dtf=True):
+
+    X['date'] = pd.to_datetime(X['date'], infer_datetime_format=infer_dtf)
+
     X['year'] = X['date'].dt.year
     X['month'] = X['date'].dt.month
     X['day'] = X['date'].dt.day
@@ -37,5 +36,3 @@ def date_sep(
     X = X.drop(columns=['date'])
 
     return X
-
-
